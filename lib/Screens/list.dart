6e9e9b12ca7f0365bd2902f0e69fb7ebe19d37dc
Antonis Giannakovitis <61454221+TonyGnk@users.noreply.github.com>
2023-11-15 12:56:19 +0200

    Fix typo in Breadth First Algorithm label

--- lib/Screens/list.dart
+++ lib/Screens/list.dart
@@ -20,7 +20,7 @@ RoutedScreen homeScreen() => const RoutedScreen(
 // The Breadth First Algorithm page.
 RoutedScreen breadthFirstAlgScreen() => const RoutedScreen(
       mainChild: BreadthFirstAlg(),
-      label: 'Αλγόριθμος Πρώτα σε Βάθος',
+      label: 'Αλγόριθμος Πρώτα σε Βάθος ',
       labelRoute: '/BreadthFirstAlgorithm',
       icon: Icons.radar_outlined,
       filledIcon: Icons.radar,
