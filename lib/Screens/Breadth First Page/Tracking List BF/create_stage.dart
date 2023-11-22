87ea98bde2acae6051c24edfa44ef3fa8c428e4b
TonyGnk <iis22067@uom.edu.gr>
2023-11-22 23:14:29 +0200

    Refactor form in main_tracking_list_bf.dart

--- lib/Screens/Breadth First Page/Tracking List BF/create_stage.dart
+++ lib/Screens/Breadth First Page/Tracking List BF/create_stage.dart
@@ -4,182 +4,133 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../Services & Providers/constants.dart';
 import '../Archive BF/list_provider.dart';
+import 'main_tracking_list_bf.dart';
 
-class SelectStage extends StatefulWidget {
-  const SelectStage({super.key});
+Widget inPutFieldBf(
+  BuildContext context,
+  TextEditingController controller,
+  String message,
+) =>
+    Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(cornerSize),
+        color: Theme.of(context).colorScheme.primary.withOpacity(0),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: message,
+        ),
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly
+        ],
+      ),
+    );
 
-  @override
-  State<SelectStage> createState() => _SelectorState();
-}
-
-class _SelectorState extends State<SelectStage> {
-  late TextEditingController controller1 = TextEditingController();
-  late TextEditingController controller2 = TextEditingController();
-
-  double currentSliderValue = 100;
-  double currentSliderValue2 = 1.0;
-
-  @override
-  Widget build(BuildContext context) => Consumer(
-      builder: (_, WidgetRef ref, __) => Column(
-            children: [
-              const SizedBox(height: 8),
-              const Text(
-                'Νέα Αναζήτηση',
-                style: TextStyle(
-                  fontSize: 17,
-                ),
-              ),
-              customFieldBf(
-                controller1,
-                'Εισάγεται Αρχική Τιμή',
-              ),
-              customFieldBf(
-                controller2,
-                'Εισάγεται Τελική Τιμή',
-              ),
-              speedSlider(context),
-              const SizedBox(height: 2),
-              solutionSlider(context),
-              Expanded(
-                child: Container(),
-              ),
-              newSubmit(context),
-            ],
-          ));
-
-  Widget newSubmit(BuildContext context) => Consumer(
-        builder: (_, WidgetRef ref, __) => Row(
+Widget speedSlider2(BuildContext context) => Consumer(
+      builder: (_, WidgetRef ref, __) {
+        final speedSlider = ref.watch(speedSliderProvider);
+        return Column(
           children: [
-            Expanded(
-              child: FilledButton.icon(
-                label: const Text('Εκτέλεση'),
-                icon: const Icon(Icons.auto_awesome_outlined),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    Theme.of(context).colorScheme.secondary,
-                  ),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(cornerSize - 1),
-                    ),
-                  ),
-                ),
-                onPressed: () => onButtonPressed(ref),
-              ),
+            const Text('Ταχύτητα Εκτέλεσης'),
+            Slider(
+              value: speedSlider,
+              max: 100,
+              divisions: 10,
+              label: speed(speedSlider),
+              thumbColor: Theme.of(context).colorScheme.secondary,
+              activeColor: Theme.of(context).colorScheme.secondary,
+              onChanged: (double value) {
+                //Change the value of speedSliderProvider
+                ref.watch(speedSliderProvider.notifier).state = value;
+              },
             ),
           ],
-        ),
-      );
-
-  void onButtonPressed(WidgetRef ref) {
-    ref.watch(bfRunningProvider.notifier).state = BfRunning(
-      startValue: int.parse(controller1.text),
-      targetValue: int.parse(controller2.text),
-      speed: setSpeedFromSlider(currentSliderValue),
+        );
+      },
     );
-    ref.watch(isCreatingProvider.notifier).state = false;
-    ref.watch(bfRunningProviderUpdater.notifier).state =
-        !ref.watch(bfRunningProviderUpdater);
+
+String speed(double currentSliderValue) {
+  if (currentSliderValue == 0) {
+    return '1 sec';
+  } else {
+    int millisec = 1000 - (currentSliderValue * 10).toInt();
+    return '$millisec millisec';
   }
+}
 
-  Widget speedSlider(BuildContext context) => Column(
-        children: [
-          const Text('Ταχύτητα Εκτέλεσης'),
-          Slider(
-            value: currentSliderValue,
-            max: 100,
-            divisions: 10,
-            label: speed(currentSliderValue),
-            thumbColor: Theme.of(context).colorScheme.secondary,
-            activeColor: Theme.of(context).colorScheme.secondary,
-            onChanged: (double value) {
-              setState(() {
-                currentSliderValue = value;
-              });
-            },
-          ),
-        ],
-      );
+//Create a provider for currentSliderValue type double with initial value 100
+final speedSliderProvider = StateProvider<double>((ref) => 100);
 
-  Widget solutionSlider(BuildContext context) => Column(
+//Create a provider for currentSliderValue2 type double with initial value 1.0
+final solutionSliderProvider = StateProvider<double>((ref) => 1.0);
+
+Widget solutionSlider2(BuildContext context) =>
+    Consumer(builder: (_, WidgetRef ref, __) {
+      final solutionSlider = ref.watch(solutionSliderProvider);
+      return Column(
         children: [
           const Text('Πλήθος Λύσεων'),
           const Text('Μη διαθέσιμο'),
           Slider(
-            value: currentSliderValue2,
+            value: solutionSlider,
             min: 1,
             max: 3,
             divisions: 2,
-            label: currentSliderValue2.toInt().toString(),
+            label: solutionSlider.toInt().toString(),
             thumbColor: Theme.of(context).colorScheme.secondary,
             activeColor: Theme.of(context).colorScheme.secondary,
             onChanged: (double value) {
-              setState(() {
-                currentSliderValue2 = value;
-              });
+              ref.watch(solutionSliderProvider.notifier).state = value;
             },
           ),
         ],
       );
+    });
 
-  String speed(double currentSliderValue) {
-    if (currentSliderValue == 0) {
-      return '1 sec';
-    } else if (currentSliderValue == 10) {
-      return '900 millisec';
-    } else if (currentSliderValue == 20) {
-      return '800 millisec';
-    } else if (currentSliderValue == 30) {
-      return '700 millisec';
-    } else if (currentSliderValue == 40) {
-      return '600 millisec';
-    } else if (currentSliderValue == 50) {
-      return '500 millisec';
-    } else if (currentSliderValue == 60) {
-      return '400 millisec';
-    } else if (currentSliderValue == 70) {
-      return '300 millisec';
-    } else if (currentSliderValue == 80) {
-      return '200 millisec';
-    } else if (currentSliderValue == 90) {
-      return '100 millisec';
-    } else {
-      return '1 millisec';
-    }
-  }
-
-  int setSpeedFromSlider(double currentSliderValue) {
-    if (currentSliderValue >= 0 && currentSliderValue <= 90) {
-      return 1000 - (currentSliderValue.toInt() * 10);
-    } else {
-      return 1;
-    }
-  }
-
-  Widget customFieldBf(
-    TextEditingController controller,
-    String message,
-  ) =>
-      Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(cornerSize),
-          color: Theme.of(context).colorScheme.primary.withOpacity(0),
-        ),
-        padding: const EdgeInsets.symmetric(horizontal: 6),
-        //margin: const EdgeInsets.all(2),
-        //width: 200,
-        //height: 38,
-        child: TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: message,
+Widget newSubmit2(BuildContext context) => Consumer(
+      builder: (_, WidgetRef ref, __) => Row(
+        children: [
+          Expanded(
+            child: FilledButton.icon(
+              label: const Text('Εκτέλεση'),
+              icon: const Icon(Icons.auto_awesome_outlined),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).colorScheme.secondary,
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(cornerSize - 1),
+                  ),
+                ),
+              ),
+              onPressed: () => onButtonPressed2(ref),
+            ),
           ),
-          keyboardType: TextInputType.number,
-          inputFormatters: <TextInputFormatter>[
-            FilteringTextInputFormatter.digitsOnly
-          ],
-        ),
-      );
+        ],
+      ),
+    );
+
+void onButtonPressed2(WidgetRef ref) {
+  final speedSlider = ref.watch(speedSliderProvider);
+  ref.watch(bfRunningProvider.notifier).state = BfRunning(
+    startValue: int.parse(controller1.text),
+    targetValue: int.parse(controller2.text),
+    speed: setSpeedFromSlider(speedSlider),
+  );
+  ref.watch(isCreatingProvider.notifier).state = false;
+  ref.watch(bfRunningProviderUpdater.notifier).state =
+      !ref.watch(bfRunningProviderUpdater);
+}
+
+int setSpeedFromSlider(double currentSliderValue) {
+  if (currentSliderValue >= 0 && currentSliderValue <= 90) {
+    return 1000 - (currentSliderValue.toInt() * 10);
+  } else {
+    return 1;
+  }
 }
--- lib/Screens/Breadth First Page/Tracking List BF/main_tracking_list_bf.dart
+++ lib/Screens/Breadth First Page/Tracking List BF/main_tracking_list_bf.dart
@@ -12,8 +12,41 @@ Widget trackingListAndButton(BuildContext context) =>
       return containerWithStyleBF(
         Theme.of(context).shadowColor.withOpacity(1),
         Theme.of(context).colorScheme.onSurface.withOpacity(0.2),
-        isCreating ? const SelectStage() : TrackingStage(ref: ref),
+        isCreating ? form(context) : TrackingStage(ref: ref),
       );
     });
 
 // Χρήσιμα Στοιχεία
+TextEditingController controller1 = TextEditingController();
+TextEditingController controller2 = TextEditingController();
+
+// Αυτή είναι η φόρμα αναζήτησης για την εισαγωγή αρχικής και τελικής τιμής
+// Περιλαμβάνει τον τίτλο, πεδία εισαγωγής και ράβδους επιλογής ταχύτητας και λύσεων
+Widget form(BuildContext context) => Column(
+      children: [
+        const SizedBox(height: 8),
+        const Text(
+          'Νέα Αναζήτηση BF',
+          style: TextStyle(
+            fontSize: 17,
+          ),
+        ),
+        inPutFieldBf(
+          context,
+          controller1,
+          'Εισάγεται Αρχική Τιμή',
+        ),
+        inPutFieldBf(
+          context,
+          controller2,
+          'Εισάγεται Τελική Τιμή',
+        ),
+        speedSlider2(context),
+        const SizedBox(height: 2),
+        solutionSlider2(context),
+        Expanded(
+          child: Container(),
+        ),
+        newSubmit2(context),
+      ],
+    );
