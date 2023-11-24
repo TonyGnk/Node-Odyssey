4ed85d59d2c6cf3247a851ce4079b7a5cdc9e973
TonyGnk <iis22067@uom.edu.gr>
2023-11-24 11:24:03 +0200

    Refactor trackingListAndButton and miniAppBar widgets

--- lib/Screens/Breadth First Page/Create & Tracking List/list_and_button_bf.dart
+++ lib/Screens/Breadth First Page/Create & Tracking List/list_and_button_bf.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../Algorithms/Breadth First/start_calculation.dart';
 import '../../../Algorithms/Breadth First/providers_bf.dart';
 import '../../../Services & Providers/tracking_container.dart';
-import 'tracking_buttons.dart';
+import '../Archive BF/list_provider.dart';
 import '../main_bf.dart';
 
-Widget trackingListAndButton(BuildContext context) => Consumer(
-    builder: (context, ref, _) => containerWithStyleBF(
-          Theme.of(context).shadowColor.withOpacity(1),
-          Theme.of(context).colorScheme.onSurface.withOpacity(0.2),
-          trackingStage(context),
-        ));
+Widget trackingListAndButton(BuildContext context) =>
+    Consumer(builder: (context, ref, _) {
+      final isAlgorithmEnd = ref.watch(isAlgorithmEndProviderBf);
+      return containerWithStyleBF(
+        Theme.of(context).shadowColor.withOpacity(1),
+        Theme.of(context).colorScheme.onSurface.withOpacity(0.2),
+        isAlgorithmEnd ? const SizedBox() : trackingStage(context),
+      );
+    });
 
 Widget trackingStage(BuildContext context) => Consumer(
       builder: (context, ref, _) {
@@ -29,36 +31,25 @@ Widget trackingStage(BuildContext context) => Consumer(
       },
     );
 
-// class trackingStage extends StatefulWidget {
-//   const trackingStage({required this.ref, super.key});
-
-//   final WidgetRef ref;
-
-//   @override
-//   State<trackingStage> createState() => _TrackingListState();
-// }
-
-// class _TrackingListState extends State<trackingStage> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     bool isFinished = widget.ref.read(isAlgorithmEndProviderBf.notifier).state;
-//     isFinished ? null : startCalR(widget.ref);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) => Consumer(
-//         builder: (context, ref, _) {
-//           final trackingList = ref.watch(trackingListProvider);
-//           // ignore: unused_local_variable
-//           final updater = ref.watch(trackUpdater);
-//           return Column(
-//             children: [
-//               miniAppBar(context),
-//               Expanded(child: trackingList.build(context)),
-//               //pauseResumeAreaBf(context),
-//             ],
-//           );
-//         },
-//       );
-// }
+Widget miniAppBar(BuildContext context) => Consumer(builder: (context, ref, _) {
+      final startValue =
+          ref.read(bfRunningProvider.notifier).state.startValue.toString();
+      final targetValue =
+          ref.read(bfRunningProvider.notifier).state.targetValue.toString();
+      return Row(
+        children: [
+          const SizedBox(width: 8),
+          Expanded(child: Text('Αναζήτηση από $startValue σε $targetValue')),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SizedBox(
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(
+                  value: 0.7,
+                )),
+          ),
+          const SizedBox(width: 8),
+        ],
+      );
+    });
